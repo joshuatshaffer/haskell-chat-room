f496ea2db3fabe1bb8d7c9645e4e1664ab70b8ba
joshuatshaffer <joshuatshaffer@gmail.com>
2017-04-17 12:52:44 -0500

    Removed event records and replaced with partial application.

--- ChatClient.hs
+++ ChatClient.hs
@@ -8,21 +8,14 @@ import Control.Concurrent
 import Control.Monad
 import Network
 
-data ClientEvent = Join String
-                 | Leave String
-                 | Broadcast String String
-                 | ChangeName String String
-                 | DirectMessage String String
-                 | WeBroadcast String
-                 | WeChangeName String
-                 | WeDirectMessage String String
+type ClientEvent = Handle -> IO ()
 
 messageToEvent :: Server2ClientMessage -> ClientEvent
-messageToEvent (S2C_J who)               = Join who
-messageToEvent (S2C_L who)               = Leave who
-messageToEvent (S2C_BC fromWhom message) = Broadcast fromWhom message
-messageToEvent (S2C_CN who newName)      = ChangeName who newName
-messageToEvent (S2C_DM fromWhom message) = DirectMessage fromWhom message
+messageToEvent (S2C_J who)               = joinEvent who
+messageToEvent (S2C_L who)               = leaveEvent who
+messageToEvent (S2C_BC fromWhom message) = broadcastEvent fromWhom message
+messageToEvent (S2C_CN who newName)      = changeNameEvent who newName
+messageToEvent (S2C_DM fromWhom message) = directMessageEvent fromWhom message
 
 messageToEvent' :: Server2ClientMessage -> MVar ClientEvent -> IO ()
 messageToEvent' m nextEvent = putMVar nextEvent $ messageToEvent m
@@ -33,21 +26,34 @@ receiveMessages h nextEvent = forever $ do
   messageToEvent' m nextEvent
 
 handleEvents :: MVar ClientEvent -> Handle -> IO ()
-handleEvents nextEvent h = forever $ takeMVar nextEvent >>= handleEvent h
+handleEvents nextEvent h = forever $ do e <- takeMVar nextEvent
+                                        e h
+joinEvent :: String -> ClientEvent
+joinEvent who _ = putStrLn $ who ++ " joined the room."
 
-handleEvent :: Handle -> ClientEvent -> IO ()
-handleEvent _ (Join who) = putStrLn $ who ++ " joined the room."
-handleEvent _ (Leave who) = putStrLn $ who ++ " left the room."
-handleEvent _ (Broadcast fromWhom message) = putStrLn $ fromWhom ++ "> " ++ message
-handleEvent _ (ChangeName who newName) = putStrLn $ who ++ " changed their name to " ++ newName
-handleEvent _ (DirectMessage fromWhom message) = putStrLn $ fromWhom ++ " says to you> " ++ message
+leaveEvent :: String -> ClientEvent
+leaveEvent who _ = putStrLn $ who ++ " left the room."
 
-handleEvent h (WeBroadcast message) = hPrint h $ C2S_BC message
-handleEvent h (WeChangeName newName) = hPrint h $ C2S_CN newName
-handleEvent h (WeDirectMessage toWhom message) = hPrint h $ C2S_DM toWhom message
+broadcastEvent :: String -> String -> ClientEvent
+broadcastEvent fromWhom message _ = putStrLn $ fromWhom ++ "> " ++ message
+
+changeNameEvent :: String -> String -> ClientEvent
+changeNameEvent who newName _ = putStrLn $ who ++ " changed their name to " ++ newName
+
+directMessageEvent :: String -> String -> ClientEvent
+directMessageEvent fromWhom message _ = putStrLn $ fromWhom ++ " says to you> " ++ message
+
+weBroadcastEvent :: String -> ClientEvent
+weBroadcastEvent message h = hPrint h $ C2S_BC message
+
+weChangeNameEvent :: String -> ClientEvent
+weChangeNameEvent newName h = hPrint h $ C2S_CN newName
+
+weDirectMessageEvent :: String -> String -> ClientEvent
+weDirectMessageEvent toWhom message h = hPrint h $ C2S_DM toWhom message
 
 handleUserInput :: MVar ClientEvent -> IO ()
-handleUserInput nextEvent = forever $ getLine >>= (putMVar nextEvent . WeBroadcast)
+handleUserInput nextEvent = forever $ getLine >>= (putMVar nextEvent . weBroadcastEvent)
 
 chatAsClient :: HostName -> PortID -> IO ()
 chatAsClient name port = do
--- ChatServer.hs
+++ ChatServer.hs
@@ -10,29 +10,24 @@ import           Network
 import           System.IO
 
 
-data ServerEvent = Join Handle String
-                 | Leave Handle
-                 | Broadcast Handle String
-                 | ChangeName Handle String
-                 | DirectMessage Handle String String
-                 deriving (Show)
+type ServerEvent = [(String, Handle)] -> IO [(String, Handle)]
 
 messageToEvent :: Client2ServerMessage -> Handle -> ServerEvent
-messageToEvent (C2S_BC message) h        = Broadcast h message
-messageToEvent (C2S_CN newName) h        = ChangeName h newName
-messageToEvent (C2S_DM toWhom message) h = DirectMessage h toWhom message
+messageToEvent (C2S_BC message) h        = broadcastEvent h message
+messageToEvent (C2S_CN newName) h        = changeNameEvent h newName
+messageToEvent (C2S_DM toWhom message) h = directMessageEvent h toWhom message
 
 messageToEvent' :: Client2ServerMessage -> Handle -> MVar ServerEvent -> IO ()
 messageToEvent' m h nextEvent = putMVar nextEvent $! messageToEvent m h
 
 serviceClient :: Handle -> MVar ServerEvent -> IO ()
 serviceClient h nextEvent =
-  void $ forkFinally (onFirstMessage >> forever onRestMessages) (\_ -> putMVar nextEvent $ Leave h)
+  void $ forkFinally (onFirstMessage >> forever onRestMessages) (\_ -> putMVar nextEvent $ leaveEvent h)
   where
     onFirstMessage = do m <- fmap read (hGetLine h)
                         case m of
-                          C2S_CN newName -> putMVar nextEvent $ Join h newName
-                          _ -> do putMVar nextEvent $! Join h ""
+                          C2S_CN newName -> putMVar nextEvent $ joinEvent h newName
+                          _ -> do putMVar nextEvent $! joinEvent h ""
                                   messageToEvent' m h nextEvent
 
     onRestMessages = do m <- fmap read (hGetLine h)
@@ -44,28 +39,31 @@ tellAllExcept hs x m = mapM_ (\(_,h) -> when (h /= x) $ hPrint h m) hs
 handleEvents :: MVar ServerEvent -> IO ()
 handleEvents nextEvent = handleEvents' []
   where handleEvents' clients = do e <- takeMVar nextEvent
-                                   newClients <- handleEvent e clients
+                                   newClients <- e clients
                                    handleEvents' newClients
 
-handleEvent :: ServerEvent -> [(String, Handle)] -> IO [(String, Handle)]
-
-handleEvent (Join h newName) clients =
+joinEvent :: Handle -> String -> ServerEvent
+joinEvent h newName clients =
   do tellAllExcept clients h $ S2C_J newName
      return $ (newName,h):clients
 
-handleEvent (Leave h) clients =
+leaveEvent :: Handle -> ServerEvent
+leaveEvent h clients =
   do tellAllExcept clients h . S2C_L . fromJust $ unlookup h clients
      return $ filter ((/= h) . snd) clients
 
-handleEvent (Broadcast h message) clients =
+broadcastEvent :: Handle -> String -> ServerEvent
+broadcastEvent h message clients =
   do tellAllExcept clients h $ S2C_BC (fromJust $ unlookup h clients) message
      return clients
 
-handleEvent (ChangeName h newName) clients =
+changeNameEvent :: Handle -> String -> ServerEvent
+changeNameEvent h newName clients =
   do tellAllExcept clients h $ S2C_CN (fromJust $ unlookup h clients) newName
      return $ (newName,h) : filter ((/= h) . snd) clients
 
-handleEvent (DirectMessage h toWhom message) clients =
+directMessageEvent :: Handle -> String -> String -> ServerEvent
+directMessageEvent h toWhom message clients =
   do hPrint (fromJust $ lookup toWhom clients) $ S2C_DM (fromJust $ unlookup h clients) message
      return clients
 
